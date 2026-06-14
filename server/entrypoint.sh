b81b06019e3033e11b72562adf8e2c28ff3bbc2d
NNGuide <nati9112.na@gmail.com>
2026-06-14 03:31:48 -0400

    Containerization tasks 2

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Make migrations and migrate the database.
 echo "Collecting static files... "
-# python manage.py makemigrations --noinput
-# python manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 exec "$@"
