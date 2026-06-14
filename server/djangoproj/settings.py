78bf75b4346cc0af60edda3449fffec56d69662d
NNGuide <nati9112.na@gmail.com>
2026-06-13 21:53:20 -0400

    Check djangp proj 6

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,11 +28,11 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 
-                 f'https://nxn210024-8000.theiadockernext-0-labs-prod- '
-                 f'theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = [f'https://nxn210024-8000.theiadockernext-0-labs-prod- '
-                        f'theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost',
+                 'https://nxn210024-8000.theiadockernext-0-labs-prod-'
+                 'theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://nxn210024-8000.theiadockernext-0-labs-prod-'
+                        'theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -95,8 +95,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        f'django.contrib.auth.password_validation.'
-        f'UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
