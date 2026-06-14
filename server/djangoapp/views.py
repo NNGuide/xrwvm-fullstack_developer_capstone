225601e72cc2dc7d0a26910730222c7026418d85
NNGuide <nati9112.na@gmail.com>
2026-06-13 20:50:33 -0400

    Check djangoapp 3

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -66,10 +66,10 @@ def registration(request):
         User.objects.get(username=username)
         username_exist = True
     except Exception as e:
+        logger.debug("{} is new user".format(username))
 	    print(f"Error: {e}")
         # If not, simply log this is a new user
-        logger.debug("{} is new user".format(username))
-
+    
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
